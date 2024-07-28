4610d692d4c302fc4e1cf54a2b17284d299630c0
Connor Szczepaniak <cszczepaniak@gmail.com>
2024-07-27 19:36:41 -0500

    build out nav

--- internal/web/components/shell_templ.go
+++ internal/web/components/shell_templ.go
@@ -26,7 +26,7 @@ func Base() templ.Component {
 			templ_7745c5c3_Var1 = templ.NopComponent
 		}
 		ctx = templ.ClearChildren(ctx)
-		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<html><head><script src=\"https://unpkg.com/htmx.org@2.0.1\"></script><script src=\"https://unpkg.com/htmx-ext-sse@2.2.1/sse.js\"></script><link rel=\"stylesheet\" href=\"/web/dist/styles.css\"></head><body>")
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<html><head><script src=\"https://unpkg.com/htmx.org@2.0.1\"></script><script src=\"https://unpkg.com/htmx-ext-sse@2.2.1/sse.js\"></script><link rel=\"stylesheet\" href=\"/web/dist/styles.css\"></head><body><nav class=\"py-2 px-4 bg-blue-500 flex flex-row\"><div class=\"flex flex-row items-center space-x-4\"><span class=\"text-3xl text-white mr-4\">Cribbly</span> <a href=\"/\" class=\"text-md text-white\">Home</a> <a href=\"/players\" class=\"text-md text-white\">Players</a></div></nav><div class=\"p-4\">")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
@@ -34,7 +34,7 @@ func Base() templ.Component {
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
-		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("</body></html>")
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("</div></body></html>")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}

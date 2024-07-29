bc9fd88c02b0da580deb0eb8b4b406143f2a1a7b
Connor Szczepaniak <cszczepaniak@gmail.com>
2024-07-28 19:17:38 -0500

    add admin routes

--- internal/http/router/routes.go
+++ internal/http/router/routes.go
@@ -4,10 +4,13 @@ import (
 	"io/fs"
 	"net/http"
 
+	"github.com/cszczepaniak/go-htmx/internal/admin"
+	"github.com/cszczepaniak/go-htmx/internal/admin/divisions"
+	"github.com/cszczepaniak/go-htmx/internal/admin/players"
+	"github.com/cszczepaniak/go-htmx/internal/admin/teams"
 	"github.com/cszczepaniak/go-htmx/internal/home"
 	"github.com/cszczepaniak/go-htmx/internal/http/httpwrap"
 	"github.com/cszczepaniak/go-htmx/internal/persistence"
-	"github.com/cszczepaniak/go-htmx/internal/players"
 	"github.com/cszczepaniak/go-htmx/internal/web/components"
 )
 
@@ -27,22 +30,41 @@ func Setup(static fs.FS, p persistence.Persistence) http.Handler {
 		components.Handler(home.Home()),
 	)
 
-	// Players
 	httpwrap.Handle(
 		m,
-		"GET /players",
+		"GET /admin",
+		components.Handler(admin.BareAdminPage()),
+	)
+
+	httpwrap.Handle(
+		m,
+		"GET /admin/players",
 		players.GetHandler(p.PlayerStore),
 	)
 	httpwrap.Handle(
 		m,
-		"POST /players",
+		"POST /admin/players",
 		players.PostHandler(p.PlayerStore),
 	)
 	httpwrap.Handle(
 		m,
-		"DELETE /players/{id}",
+		"DELETE /admin/players/{id}",
 		players.DeleteHandler(p.PlayerStore),
 	)
+
+	httpwrap.Handle(
+		m,
+		"GET /admin/teams",
+		teams.GetHandler(),
+	)
+
+	httpwrap.Handle(
+		m,
+		"GET /admin/divisions",
+		divisions.GetHandler(),
+	)
+
+	// Players
 
 	return m
 }
